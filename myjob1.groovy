c895b34d9a63d0b6966879c8bbce7b503c0d4f96
mathias <mathias.lachot@eemi.com>
2019-05-05 22:11:05 +0200

    Update myjob1.groovy

--- myjob1.groovy
+++ myjob1.groovy
@@ -2,7 +2,7 @@ job('My Job 1') {
     scm {
         git('https://github.com/Mathiaslachot/docker_jenkins.git') {  node -> 
             node / gitConfigName('mlachot  ')
-            node / gitConfigEmail('mathias.lachot@orange.fr')
+            node / gitConfigEmail('mathias.lachot@eemi.com)
         }
     }
     triggers {
